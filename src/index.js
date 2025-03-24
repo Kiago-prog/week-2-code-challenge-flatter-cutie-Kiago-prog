4822d57a60124591047db86fcae9cc752f88063b
kiago <roy.kiago@student.moringaschool.com>
2025-03-24 10:52:06 +0300

    getElements

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ let currentCharacter = null;
 
 // Wait for the DOM to load before executing code
 document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('DOMContentLoaded', () => {
+        // Get elements from the DOM
+        const characterBar = document.getElementById('character-bar');
+        const detailedInfo = document.getElementById('detailed-info');
+        const nameEl = document.getElementById('name');
+        const imageEl = document.getElementById('image');
+        const voteCountEl = document.getElementById('vote-count');
+        const votesForm = document.getElementById('votes-form');
+        const resetBtn = document.getElementById('reset-btn');
